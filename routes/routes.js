286cb059595cf80e646c1af065e6efe29e9b52fc
remiyeku <antakin4@gmail.com>
2023-01-17 10:50:19 +0000

    broken tests begun, 500 errors

--- routes/routes.js
+++ routes/routes.js
@@ -4,16 +4,15 @@ import getDataByCoords from "../models/model.js";
 const router = express.Router();
 
 //get by coordinates
-router.get('/', async (req, res) => {
-    try{
-        console.log("made it to routes")
-        const bars = await getDataByCoords(req.body)
-        console.log(bars)
-        res.send(bars);
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    console.log("made it to routes");
+    const bars = await getDataByCoords(req.body);
+    console.log(bars);
+    res.send({ success: true, payload: bars });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
