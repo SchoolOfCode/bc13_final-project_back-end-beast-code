b6d2f5716ecc4458fa2cf58bee656b5a3c0d2bf3
keirastanley <keiraestanley@gmail.com>
2023-01-19 12:26:10 +0100

    Post request to handle filters written

--- models/model.js
+++ models/model.js
@@ -1,33 +1,48 @@
-import {bardata} from "../app.js";
+import { bardata } from "../app.js";
 
-export default async function getDataByCoords(arrayCoords, filterQuery) {
-    console.log("made it to models")
-    console.log(filterQuery)
-    let queryObj = {}
-
-    for (let i = 0; i < filterQuery.length; i++) {
-      const property = Object.values(filterQuery[i])[0]
-      const value = Object.values(filterQuery[i])[1]
-      if (value.length === 1) {
-        queryObj[property] = value[0];
-      } 
-      else {
-        queryObj[property] = {$in: value};
-      }
-      console.log("HIII", queryObj)
+export async function getDataByCoords(arrayCoords) {
+  const data = await bardata.aggregate([{
+    '$geoNear': {
+      'near': {
+        'type': 'Point',
+        'coordinates': arrayCoords
+      },
+      'distanceField': 'dist.calculated',
+      'maxDistance': 20000,
+      'query': {},
+      'spherical': true
     }
-    const data = await bardata.aggregate([{
-          '$geoNear': {
-            'near': {
-              'type': 'Point', 
-              'coordinates': arrayCoords
-            }, 
-            'distanceField': 'dist.calculated', 
-            'maxDistance': 20000, 
-            'query': queryObj, 
-            'spherical': true
-          }
-    }]);
-    const ans = await data.toArray();
-    return ans;
+  }]);
+  const ans = await data.toArray();
+  return ans;
 };
+
+export async function getFilteredData(arrayCoords, filterQueryObject) {
+  const filterQuery = filterQueryObject.queryFilters
+  //Builds the query object
+  let queryObj = {}
+  for (let i = 0; i < filterQuery.length; i++) {
+    const property = Object.values(filterQuery[i])[0]
+    const value = Object.values(filterQuery[i])[1]
+    if (value.length === 1) {
+      queryObj[property] = value[0];
+    }
+    else {
+      queryObj[property] = { $in: value };
+    }
+  }
+  const data = await bardata.aggregate([{
+    '$geoNear': {
+      'near': {
+        'type': 'Point',
+        'coordinates': arrayCoords
+      },
+      'distanceField': 'dist.calculated',
+      'maxDistance': 20000,
+      'query': queryObj,
+      'spherical': true
+    }
+  }]);
+  const ans = await data.toArray();
+  return ans;
+}
